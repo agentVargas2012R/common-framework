220b5349295299714167ae947641a45485ef6246
michael.vargas <michael.vargas@rci.com>
2025-03-28 16:32:56 -0400

    java change

--- src/main/java/com/wyn/models/User.java
+++ src/main/java/com/wyn/models/User.java
@@ -1,6 +1,8 @@
 package com.wyn.models;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private long id;
     private String username;
     private String email;
